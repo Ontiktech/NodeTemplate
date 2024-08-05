29c7186468b2650216500abe0649340fc119e8bf
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-08-05 09:28:15 +0600

    Added a global rate limiter.

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,10 @@ import * as bodyParser from 'body-parser';
 import path from 'path';
 import cors from 'cors'
 import helmet from "helmet";
-// import {connectMongoos} from './db/clients/mondo.client';
+import {connectMongoos} from './db/clients/mondo.client';
 import expressListRoutes from 'express-list-routes';
-import { corsOptions } from './config/cors';
+import { corsOptions } from './config/cors.config';
+import { globalLimiterOptions } from 'config/globalRateLimiter.config';
 
 const numCPUs = os.cpus().length
 
@@ -44,7 +45,10 @@ const server = () => {
             //serve static files
             app.use('/', express.static(path.join(__dirname, '/public')));
 
-            // connectMongoos()
+            //rate limiter
+            app.use(globalLimiterOptions)
+
+            connectMongoos()
 
             app.use('/api/v1/user', router)
 
